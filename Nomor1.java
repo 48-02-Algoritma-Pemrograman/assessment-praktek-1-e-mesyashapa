60efd6ce9323281837d6fffe1222d897e10bf53b
mesyashapa <mesyashapaa@gmail.com>
2024-10-24 13:57:11 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Nomor1{
     public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
 
+   System.out.print("Masukkan jumlah anggota keluarga (1-3 jiwa): ");
+        int jumlahAnggota = scanner.nextInt();
+
+    double tunjanganKesehatan;
+
+    if (jumlahAnggota <= 3) {
+        tunjanganKesehatan = jumlahAnggota * 100000;
+        } else {
+        tunjanganKesehatan = (3 * 100000) + ((jumlahAnggota - 3 ) * 50000);
     }
+
+System.out.println("Besar Tunjangan Kesehatan = " + tunjanganKesehatan); 
+    }
+
 }
