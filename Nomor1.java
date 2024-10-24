99fd90e4494b60e8894e30e22d82288c7bfda37f
mesyashapa <mesyashapaa@gmail.com>
2024-10-24 14:34:55 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Nomor1{
     public static void main(String[] args){
+
     Scanner scanner = new Scanner(System.in);
 
    System.out.print("Masukkan jumlah anggota keluarga (1-3 jiwa): ");
@@ -15,7 +16,20 @@ public class Nomor1{
         tunjanganKesehatan = (3 * 100000) + ((jumlahAnggota - 3 ) * 50000);
     }
 
-System.out.println("Besar Tunjangan Kesehatan = " + tunjanganKesehatan); 
+System.out.println("Besar Tunjangan Kesehatan = " + tunjanganKesehatan);
+
+
+        
     }
 
 }
+
+//PSEUDOCODE
+//Start
+//Input jumlah anggota keluarga
+//If jumlah anggota keluarga <= 3, Else
+//tunjangan kesehatan = jumlah anggota keluarga * 100.000
+//If jumlah anggota keluarga > 3, Else
+//tunjangan kesehatan = (3 * 100.000) + ((jumlah anggota keluarga - 3) * 50.000)
+//Output tunjangan kesehatan
+//Selesai
